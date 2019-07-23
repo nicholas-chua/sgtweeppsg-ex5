16a19ee3126d1cc32a982af0160e0a591df0264d
nicholaschua05 <37202174+nicholaschua05@users.noreply.github.com>
2019-07-23 11:42:08 +0800

    Add files via upload

---
+++ shang5b.py
@@ -0,0 +1,15 @@
+import csv
+import itertools
+data = dict()
+reader = csv.reader(open("avocado.csv"))
+next(reader)
+
+for row in reader:
+    region = row[13]
+    if region not in data.keys():
+        data[region] = list()
+    data[region].append(float(row[2]))
+
+for region, prices in data.items():
+    print(region+"------------------------------------")
+    print(sum(prices)/len(prices))
